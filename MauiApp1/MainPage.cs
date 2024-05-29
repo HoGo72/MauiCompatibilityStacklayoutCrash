2541845bb552545e0ac9ae3aa1d1774283ac081e
Holger Gothan <h.gothan@friedrich-datentechnik.de>
2024-05-29 11:55:38 +0200

    Workaround Android crash Sample for iOS crash

--- MauiApp1/MainPage.cs
+++ MauiApp1/MainPage.cs
@@ -1,5 +1,4 @@
-﻿
-//using CStackLayout = Microsoft.Maui.Controls.StackLayout;
+﻿//using CStackLayout = Microsoft.Maui.Controls.StackLayout;
 using CStackLayout=Microsoft.Maui.Controls.Compatibility.StackLayout;
 
 namespace MauiApp1
@@ -7,7 +6,9 @@ namespace MauiApp1
 
     public class MainPage : ContentPage
     {
-        View m_Collection;
+        CollectionView m_Collection;
+        CStackLayout m_MainSubView;
+        CStackLayout m_SearchView;
         Button m_Button;
         CStackLayout m_MainView;
 
@@ -15,7 +16,21 @@ namespace MauiApp1
         {
             m_Collection = new CollectionView
             {
-                ItemsSource = new List<string>() { "aaa", "bbb", "ccc" },
+                ItemsSource = new List<string>() { "aaa", "bb", "c" }
+            };
+
+            m_SearchView = new CStackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children = 
+                { 
+                    new Label { Text="Items" }
+                }
+            };
+
+            m_MainSubView = new CStackLayout
+            {
+                Children = { m_SearchView, m_Collection }
             };
 
             m_Button = new Button { Text = "Update", Command = new Command(OnUpdate) };
@@ -26,12 +41,20 @@ namespace MauiApp1
 
         void AddContentNow()
         {
-            m_MainView.Children.Add(m_Collection);
+            m_MainView.Children.Add(m_MainSubView);
             m_MainView.Children.Add(m_Button);
         }
 
         void OnUpdate()
         {
+#if ANDROID // Workaround for System.ObjectDisposedException: 'Cannot access a disposed object. Object name: 'Microsoft.Maui.Controls.Compatibility.Platform.Android.Platform+DefaultRenderer'.'
+            m_SearchView.Handler.DisconnectHandler();
+            m_MainSubView.Handler.DisconnectHandler();
+#endif
+
+#if IOS // Workaround for System.ArgumentNullException: 'Value cannot be null. (Parameter 'view')' ???
+#endif
+
             m_MainView.Children.Clear();
             AddContentNow();
         }
